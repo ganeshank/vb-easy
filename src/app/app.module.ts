b38c031742e51f3d36a47ba3a02f7b42afd515b3
ganeshan <ganeshank04@gmail.com>
2018-07-16 23:28:45 +0530

    new project added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
+import { LoginService } from './services/login.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  // { path: '', 
+  //   redirectTo: '/login',
+  //   pathMatch: 'full' }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
